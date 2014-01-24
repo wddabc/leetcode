0668153b0eae6a9836e1b64878b83448c7b151fa
wddabc <wddabc@gmail.com>
2014-01-24 01:13:54 -0500

    Fri Jan 24 01:13:54 EST 2014

---
+++ gen_folder.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+# --------------------------------------- 
+# File Name : gen_folder1.sh
+# Creation Date : 24-01-2014
+# Last Modified : Fri Jan 24 01:12:14 2014
+# Created By : wdd 
+# --------------------------------------- 
+array=($(ls | grep -v "test.cpp" | grep ".*\.cpp$"))
+for d in ${array[*]}
+do
+	newname=$(echo $d | sed 's/.cpp//g')
+	echo ${newname}
+	mkdir ${newname}
+	mv $d ${newname}/sol1.cpp
+	git add ${newname}
+	git add ${newname}/sol1.cpp
+	git rm $d
+done
+git commit -m "$(date)"
+git push
