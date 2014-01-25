7443d985f3bea1def352c944aa58a7c727cd83f4
wddabc <wddabc@gmail.com>
2014-01-25 18:49:37 -0500

    Sat Jan 25 18:49:37 EST 2014

--- Add_Two_Numbers/sol1.cpp
+++ Add_Two_Numbers/sol1.cpp
@@ -2,41 +2,30 @@
 * File Name : Add_Two_Numbers.cpp
 * Purpose :
 * Creation Date : 17-12-2013
-* Last Modified : Tue Dec 17 02:52:59 2013
+* Last Modified : Sat Jan 25 18:49:13 2014
 * Created By : wdd 
 _._._._._._._._._._._._._._._._._._._._._.*/
 #include "general.h"
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-	};
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-	ListNode *ln = new ListNode(0); 
-	ListNode *curln = ln;
-	int adv = 0, rem = 0;
-	while(1){
-		int n1 = adv, n2 = 0;
-		bool l1v = 0, l2v = 0;
-		if(l1){
-			n1 += l1->val;
-			l1 = l1->next;
-			l1v = 1;
-		}
-		if(l2){
-			n2 += l2->val;
-			l2 = l2->next;
-			l2v = 1;
-		}
-		int sum = n1 + n2;
-		adv = sum/10;
-		rem = sum%10;
-		if (!(l1v||l2v||rem)) break;
-		curln->next = new ListNode(rem);
-		curln = curln->next;
-	}
-	return ln->next;
-}
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode dummy(0), *ptr = &dummy;
+        int rem = 0;
+        while(l1 || l2 || rem){
+            int tmp_sum = rem;
+            if(l1) {
+                tmp_sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2) {
+                tmp_sum += l2->val;
+                l2 = l2->next;
+            }
+            rem = tmp_sum/10;
+            tmp_sum %= 10;
+            ptr->next = new ListNode(tmp_sum);
+            ptr = ptr->next;
+        }
+        return dummy.next;
+    }
 int main(int argc, const char* argv[])
 {
 	cout << addTwoNumbers(new ListNode(0), new ListNode(0))->val << endl;
