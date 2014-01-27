21b081da9aad1afd646650d303c10af05bfde532
wddabc <wddabc@gmail.com>
2014-01-26 19:34:01 -0500

    Sun Jan 26 19:34:01 EST 2014

--- Integer_to_Roman/sol1.cpp
+++ Integer_to_Roman/sol1.cpp
@@ -2,7 +2,7 @@
 * File Name : Integer_to_Roman.cpp
 * Purpose :
 * Creation Date : 03-01-2014
-* Last Modified : Fri Jan  3 18:56:19 2014
+* Last Modified : Sun Jan 26 19:33:42 2014
 * Created By : wdd 
 _._._._._._._._._._._._._._._._._._._._._.*/
 #include "general.h"
@@ -25,18 +25,18 @@ _._._._._._._._._._._._._._._._._._._._._.*/
             num/=10;
             string tmp = "";
             if(!dig) {itr ++; continue;}
-            if (dig < 4) {
+            if (dig < 4) { // append single Roman
                 for (int i = 0; i < dig; ++ i)tmp += dict[itr][0];
             }
-            else if(dig == 4){
+            else if(dig == 4){ // put "1" on the left hand side of "5"
                 tmp += dict[itr][0];
                 tmp += dict[itr][1];
             }
-            else if(dig > 4 && dig < 9) {
+            else if(dig > 4 && dig < 9) {// put "5", then "1"
                 tmp += dict[itr][1];
                 for (int i = 0; i < (dig - 5); ++ i)tmp += dict[itr][0];
             }
-            else if(dig == 9){
+            else if(dig == 9){ //put "1" on the left hand side of "10"
                 tmp += dict[itr][0];
                 tmp += dict[itr][2];
             }
