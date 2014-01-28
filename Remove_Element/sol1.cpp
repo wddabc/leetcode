93db9105def58d72589fc16fed6d21a98c21ecde
wddabc <wddabc@gmail.com>
2014-01-27 21:38:44 -0500

    Mon Jan 27 21:38:44 EST 2014

---
+++ Remove_Element/sol1.cpp
@@ -0,0 +1,19 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : sol1.cpp
+* Purpose :
+* Creation Date : 27-01-2014
+* Last Modified : Mon Jan 27 21:33:50 2014
+* Created By : wdd 
+_._._._._._._._._._._._._._._._._._._._._.*/
+#include "general.h"
+    int removeElement(int A[], int n, int elem) {
+        int idx = 0, anchor = 0;
+        for (int i = 0; i < n; ++ i)
+            if(A[i] != elem)
+                A[anchor++] = A[i];
+        return anchor;
+    }
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
