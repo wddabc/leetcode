e9888681f636dc0922a9e8c0c72cfb919b6b45fe
Dingquan Wang <wddabc@gmail.com>
2014-11-16 17:56:22 -0500

    Sun Nov 16 17:56:22 EST 2014

---
+++ general.h
@@ -0,0 +1,21 @@
+#ifndef _GENERAL_
+#define _GENERAL_
+#include<cstdio>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include<string>
+#include<ctime>
+#include<map>
+#include<set>
+#include<iostream>
+#include<sstream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<cmath>
+#include <sys/stat.h>
+using namespace std;
+#include "macro.h"
+#endif
